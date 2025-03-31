7cb86f9df700d39cd95ac001bb407be7d680f112
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2025-03-31 21:36:24 +0530

    Add files via upload

---
+++ Practical3.R
@@ -0,0 +1,8 @@
+# Find Sum, Mean and Product of Vector 
+A=c(30,NULL,40,20,NA)
+print("Sum is ")
+print(sum(A,na.rm=TRUE))
+print("Mean is")
+print(mean(A,na.rm=TRUE))
+print("Profuct is:")
+print(prod(A,na.rm=TRUE))
