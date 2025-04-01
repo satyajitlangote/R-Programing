ec9e0a9dfbb7beddb2d596ea35dc6d4d8341205c
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2025-04-01 22:11:05 +0530

    Add files via upload

---
+++ Practical10.R
@@ -0,0 +1,7 @@
+# Program to check if the input number is odd or even
+num=as.integer(readline(prompt = "Enter a numnber "))
+if((num %% 2) == 0) {
+  print(paste(num,"is Even"))
+} else {
+  print(paste(num,"is Odd"))
+}
