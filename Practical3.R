5c70763a53e35475e9a5791f7f1573b2f80c20dc
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2025-04-02 22:58:30 +0530

    Add files via upload

--- Practical3.R
+++ Practical3.R
@@ -1,8 +1,10 @@
-# Find Sum, Mean and Product of Vector 
-A=c(30,NULL,40,20,NA)
-print("Sum is ")
-print(sum(A,na.rm=TRUE))
-print("Mean is")
-print(mean(A,na.rm=TRUE))
-print("Profuct is:")
-print(prod(A,na.rm=TRUE))
+#Create a bar graph, that shows the number of each gear type in mtcars. 
+#Freq of gear type 
+gearType<-table(mtcars$gear) 
+View(gearType) 
+#vector of names appearing under each bar 
+xVal<-c("Type 3", "Type 4","Type 5") 
+# Plot the bar chart  
+barplot(gearType,names.arg=xVal,xlab="Gear Types",ylab="Total # of each 
+type",col="green", 
+        main="Distribution Of Gear Types",border="black") 
